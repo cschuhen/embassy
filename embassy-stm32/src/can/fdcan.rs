378b81eb6a9015498d66f6bc27060f54e87ba369
Corey Schuhen <cschuhen@gmail.com>
2024-01-26 11:02:15 +1000

    Use broken out definitions that can be shared with bxcan

--- embassy-stm32/src/can/fdcan.rs
+++ embassy-stm32/src/can/fdcan.rs
@@ -18,6 +18,10 @@ use crate::interrupt::typelevel::Interrupt;
 use crate::rcc::RccPeripheral;
 use crate::{interrupt, peripherals, Peripheral};
 
+pub mod enums;
+use enums::*;
+pub mod util;
+
 /// CAN Frame returned by read
 pub struct RxFrame {
     /// CAN Header info: frame ID, data length and other meta
@@ -178,34 +182,6 @@ impl<T: Instance> interrupt::typelevel::Handler<T::IT1Interrupt> for IT1Interrup
     unsafe fn on_interrupt() {}
 }
 
-/// Bus error
-#[derive(Debug)]
-pub enum BusError {
-    /// Bit stuffing error - more than 5 equal bits
-    Stuff,
-    /// Form error - A fixed format part of a received message has wrong format
-    Form,
-    /// The message transmitted by the FDCAN was not acknowledged by another node.
-    Acknowledge,
-    /// Bit0Error: During the transmission of a message the device wanted to send a dominant level
-    /// but the monitored bus value was recessive.
-    BitRecessive,
-    /// Bit1Error: During the transmission of a message the device wanted to send a recessive level
-    /// but the monitored bus value was dominant.
-    BitDominant,
-    /// The CRC check sum of a received message was incorrect. The CRC of an
-    /// incoming message does not match with the CRC calculated from the received data.
-    Crc,
-    /// A software error occured
-    Software,
-    ///  The FDCAN is in Bus_Off state.
-    BusOff,
-    ///  The FDCAN is in the Error_Passive state.
-    BusPassive,
-    ///  At least one of error counter has reached the Error_Warning limit of 96.
-    BusWarning,
-}
-
 impl BusError {
     fn try_from(lec: LastErrorCode) -> Option<BusError> {
         match lec {
@@ -232,105 +208,6 @@ impl FdcanOperatingMode for fdcan::NormalOperationMode {}
 impl FdcanOperatingMode for fdcan::RestrictedOperationMode {}
 impl FdcanOperatingMode for fdcan::BusMonitoringMode {}
 impl FdcanOperatingMode for fdcan::TestMode {}
-
-fn calc_fdcan_timings(periph_clock: crate::time::Hertz, can_bitrate: u32) -> Option<config::NominalBitTiming> {
-    const BS1_MAX: u8 = 16;
-    const BS2_MAX: u8 = 8;
-    const MAX_SAMPLE_POINT_PERMILL: u16 = 900;
-
-    let periph_clock = periph_clock.0;
-
-    if can_bitrate < 1000 {
-        return None;
-    }
-
-    // Ref. "Automatic Baudrate Detection in CANopen Networks", U. Koppe, MicroControl GmbH & Co. KG
-    //      CAN in Automation, 2003
-    //
-    // According to the source, optimal quanta per bit are:
-    //   Bitrate        Optimal Maximum
-    //   1000 kbps      8       10
-    //   500  kbps      16      17
-    //   250  kbps      16      17
-    //   125  kbps      16      17
-    let max_quanta_per_bit: u8 = if can_bitrate >= 1_000_000 { 10 } else { 17 };
-
-    // Computing (prescaler * BS):
-    //   BITRATE = 1 / (PRESCALER * (1 / PCLK) * (1 + BS1 + BS2))       -- See the Reference Manual
-    //   BITRATE = PCLK / (PRESCALER * (1 + BS1 + BS2))                 -- Simplified
-    // let:
-    //   BS = 1 + BS1 + BS2                                             -- Number of time quanta per bit
-    //   PRESCALER_BS = PRESCALER * BS
-    // ==>
-    //   PRESCALER_BS = PCLK / BITRATE
-    let prescaler_bs = periph_clock / can_bitrate;
-
-    // Searching for such prescaler value so that the number of quanta per bit is highest.
-    let mut bs1_bs2_sum = max_quanta_per_bit - 1;
-    while (prescaler_bs % (1 + bs1_bs2_sum) as u32) != 0 {
-        if bs1_bs2_sum <= 2 {
-            return None; // No solution
-        }
-        bs1_bs2_sum -= 1;
-    }
-
-    let prescaler = prescaler_bs / (1 + bs1_bs2_sum) as u32;
-    if (prescaler < 1) || (prescaler > 1024) {
-        return None; // No solution
-    }
-
-    // Now we have a constraint: (BS1 + BS2) == bs1_bs2_sum.
-    // We need to find such values so that the sample point is as close as possible to the optimal value,
-    // which is 87.5%, which is 7/8.
-    //
-    //   Solve[(1 + bs1)/(1 + bs1 + bs2) == 7/8, bs2]  (* Where 7/8 is 0.875, the recommended sample point location *)
-    //   {{bs2 -> (1 + bs1)/7}}
-    //
-    // Hence:
-    //   bs2 = (1 + bs1) / 7
-    //   bs1 = (7 * bs1_bs2_sum - 1) / 8
-    //
-    // Sample point location can be computed as follows:
-    //   Sample point location = (1 + bs1) / (1 + bs1 + bs2)
-    //
-    // Since the optimal solution is so close to the maximum, we prepare two solutions, and then pick the best one:
-    //   - With rounding to nearest
-    //   - With rounding to zero
-    let mut bs1 = ((7 * bs1_bs2_sum - 1) + 4) / 8; // Trying rounding to nearest first
-    let mut bs2 = bs1_bs2_sum - bs1;
-    core::assert!(bs1_bs2_sum > bs1);
-
-    let sample_point_permill = 1000 * ((1 + bs1) / (1 + bs1 + bs2)) as u16;
-    if sample_point_permill > MAX_SAMPLE_POINT_PERMILL {
-        // Nope, too far; now rounding to zero
-        bs1 = (7 * bs1_bs2_sum - 1) / 8;
-        bs2 = bs1_bs2_sum - bs1;
-    }
-
-    // Check is BS1 and BS2 are in range
-    if (bs1 < 1) || (bs1 > BS1_MAX) || (bs2 < 1) || (bs2 > BS2_MAX) {
-        return None;
-    }
-
-    // Check if final bitrate matches the requested
-    if can_bitrate != (periph_clock / (prescaler * (1 + bs1 + bs2) as u32)) {
-        return None;
-    }
-
-    // One is recommended by DS-015, CANOpen, and DeviceNet
-    let sync_jump_width = core::num::NonZeroU8::new(1)?;
-
-    let seg1 = core::num::NonZeroU8::new(bs1)?;
-    let seg2 = core::num::NonZeroU8::new(bs2)?;
-    let nz_prescaler = core::num::NonZeroU16::new(prescaler as u16)?;
-
-    Some(config::NominalBitTiming {
-        sync_jump_width,
-        prescaler: nz_prescaler,
-        seg1,
-        seg2,
-    })
-}
 
 /// FDCAN Instance
 pub struct Fdcan<'d, T: Instance, M: FdcanOperatingMode> {
@@ -363,7 +240,6 @@ impl<'d, T: Instance> Fdcan<'d, T, fdcan::ConfigMode> {
 
         T::configure_msg_ram();
         unsafe {
-
             T::IT0Interrupt::unpend(); // Not unsafe
             T::IT0Interrupt::enable();
 
@@ -386,8 +262,13 @@ impl<'d, T: Instance> Fdcan<'d, T, fdcan::ConfigMode> {
 
     /// Configures the bit timings calculated from supplied bitrate.
     pub fn set_bitrate(&mut self, bitrate: u32) {
-        let bit_timing = calc_fdcan_timings(T::frequency(), bitrate).unwrap();
-        self.can.borrow_mut().set_nominal_bit_timing(bit_timing);
+        let bit_timing = util::calc_can_timings(T::frequency(), bitrate).unwrap();
+        self.can.borrow_mut().set_nominal_bit_timing(config::NominalBitTiming {
+            sync_jump_width: bit_timing.sync_jump_width,
+            prescaler: bit_timing.prescaler,
+            seg1: bit_timing.seg1,
+            seg2: bit_timing.seg2,
+        });
     }
 }
 
